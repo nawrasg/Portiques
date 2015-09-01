c84bcaafec0dde47004548b68ac4a611c1bb9fad
Nawras GEORGI <support@nawrasg.fr>
2015-09-01 10:28:58 +0200

    Implement login controller

--- client/angularjs/app/scripts/controllers/login.js
+++ client/angularjs/app/scripts/controllers/login.js
@@ -6,6 +6,20 @@
  * @description # LoginCtrl Controller of the angularjsApp
  */
 
-nApp.controller('LoginCtrl', function($scope) {
-	
+nApp.controller('LoginCtrl', function($scope, $http, base64, $sessionStorage, $rootScope, $location, PortiquesUri) {
+	$scope.auth = function(){
+		var nUsername = $scope.user.username;
+		var nPwd = $scope.user.password;
+		var nURL = PortiquesUri.Login();
+		$http.defaults.headers.common.Authorization = base64.encode(nUsername + ':' + nPwd);
+		$http.get(nURL).success(function(data, status){
+			if(status == 202){
+				$sessionStorage.user = data;
+				delete $http.defaults.headers.common.Authorization;
+				$location.path('/');
+			}else{
+				console.log(status, data);
+			}
+		});
+	};
 });
