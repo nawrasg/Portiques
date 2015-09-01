8931ab030c1e4135032cd6c8bff8fd0c55182611
Nawras GEORGI <support@nawrasg.fr>
2015-09-01 11:02:08 +0200

    Implement National controller

--- client/angularjs/app/scripts/controllers/national.js
+++ client/angularjs/app/scripts/controllers/national.js
@@ -3,15 +3,20 @@
 /**
  * @ngdoc function
  * @name angularjsApp.controller:NationalCtrl
- * @description
- * # NationalCtrl
- * Controller of the angularjsApp
+ * @description # NationalCtrl Controller of the angularjsApp
  */
-angular.module('angularjsApp')
-  .controller('NationalCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+nApp.controller('NationalCtrl', function($scope, $http, $sessionStorage, PortiquesUri) {
+	getGare();
+	
+	$scope.getLink = function(point){
+		return '#local/' + point.id;
+	};
+	function getGare (){
+		var nURL = PortiquesUri.Gares();
+		$http.get(nURL).success(function(data, status, header, config){
+			$scope.points = data;
+			$sessionStorage.gares = data;
+		});
+	}
+});
