9aac0e391d425080b9fb0cf95222803c6d6ab52c
Nawras GEORGI <support@nawrasg.fr>
2015-09-01 10:20:58 +0200

    Add base64 dependency

--- client/angularjs/test/karma.conf.js
+++ client/angularjs/test/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/ngstorage/ngStorage.js',
+      'bower_components/angular-utf8-base64/angular-utf8-base64.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
