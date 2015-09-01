4bcabc47bd69596af31051d434cb1c20263ce5d1
Nawras GEORGI <support@nawrasg.fr>
2015-09-01 10:07:00 +0200

    Add login controller

---
+++ client/angularjs/app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the angularjsApp
+ */
+angular.module('angularjsApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
