c0235ebea274f2afb6e0ad6cb0244c223700ae06
Nawras GEORGI <support@nawrasg.fr>
2015-09-01 10:14:12 +0200

    Add ngStorage module

--- client/angularjs/app/scripts/app.js
+++ client/angularjs/app/scripts/app.js
@@ -8,19 +8,18 @@
  *
  * Main module of the application.
  */
-var nApp = angular.module('angularjsApp', ['ngRoute', 'ngSanitize', 'ngMaterial']);
+var nApp = angular.module('angularjsApp', ['ngRoute', 'ngSanitize', 'ngMaterial', 'ngStorage']);
   
-nApp.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+
+nApp.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'views/main.html',
+		controller : 'MainCtrl'
+	}).when('/login', {
+		templateUrl : 'views/login.html',
+		controller : 'LoginCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+});
+
