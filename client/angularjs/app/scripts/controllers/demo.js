820ae4aaad081e146bc615a55cb7fbc595a00420
Nawras GEORGI <support@nawrasg.fr>
2015-09-02 14:48:23 +0200

    Implement dashboard saving mechanism

--- client/angularjs/app/scripts/controllers/demo.js
+++ client/angularjs/app/scripts/controllers/demo.js
@@ -3,15 +3,13 @@
 /**
  * @ngdoc function
  * @name angularjsApp.controller:DemoCtrl
- * @description
- * # DemoCtrl
- * Controller of the angularjsApp
+ * @description # DemoCtrl Controller of the angularjsApp
  */
-angular.module('angularjsApp')
-  .controller('DemoCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+nApp.controller('DemoCtrl', function($scope, $localStorage) {
+	var model = $localStorage.dashboard;
+	$scope.model = model;
+	$scope.$on('adfDashboardChanged', function(event, name, model){
+		$localStorage.dashboard = model;
+	});
+});
