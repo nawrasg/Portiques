f96efe554a56e5dc07e104b000edd642dafc11b5
Nawras GEORGI <support@nawrasg.fr>
2015-09-03 16:10:34 +0200

    .

--- client/widgets/widget electricity/src/Electricity.js
+++ client/widgets/widget electricity/src/Electricity.js
@@ -1,14 +1,18 @@
 'use strict';
 
-angular.module('adf.widget.Electricity', ['adf.provider'])
-  .config(function(dashboardProvider){
-    dashboardProvider
-      .widget('Electricity', {
-        title: 'Electricity',
-        description: 'Affiche la consommation électrique des portiques.',
-        templateUrl: '{widgetsPath}/Electricity/src/view.html',
-        edit: {
-          templateUrl: '{widgetsPath}/Electricity/src/edit.html'
-        }
-      });
-  });
+var ajs = angular.module('adf.widget.Electricity', [ 'adf.provider', 'ngMaterial', 'chart.js' ]);
+
+ajs.config(function(dashboardProvider) {
+	dashboardProvider.widget('Electricity', {
+		title : 'Electricity',
+		description : 'Affiche la consommation électrique des portiques.',
+		templateUrl : '{widgetsPath}/Electricity/src/view.html',
+		edit : {
+			templateUrl : '{widgetsPath}/Electricity/src/edit.html'
+		}
+	});
+});
+
+ajs.controller('CurrentCtrl', function($scope){
+	
+});
